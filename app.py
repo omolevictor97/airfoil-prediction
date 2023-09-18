bebfc65690587210d530eaadb08f4acc4789a161
omolevictor97Omole Victor Opeyemi <omolevictor97@gmail.com>
2023-09-18 21:53:34 +0100

    Styles Added

--- app.py
+++ app.py
@@ -15,6 +15,27 @@ def predict():
     if request.method == "GET":
         return render_template("home.html")
     else:
+
+        frequency = float(request.form.get("frequency"))
+        angle = float(request.form.get("angle"))
+        chord_length = float(request.form.get("Chord length"))
+        velocity = float(request.form.get("Velocity"))
+        suction_side = float(request.form.get("Suction side"))
+
+        if frequency == "" or angle == "" or chord_length == "" or velocity == "" or suction_side == "":
+            flash("One or more empty fields")
+            return render_template("home.html")
+        else:
+            model = pickle.load(open("model_unscaled.pkl", "rb"))
+            arr = np.array([[frequency, angle, chord_length, velocity, suction_side]])
+            pred = model.predict(arr)
+            return render_template("result.html", data=np.round(pred,2))
+        
+@app.route("/predict_postman", methods=["GET", "POST"])
+def predict():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
         
         frequency = float(request.form.get("frequency"))
         angle = float(request.form.get("angle"))
